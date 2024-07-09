6d0c512232ff1e7474a5164552d737c9eea41659
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-07-10 00:52:04 +0330

    js 04 pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...entries) {
+  let result = array;
+  for (const entry of entries) {
+    result = result.filter((a) => { return (!(a === entry)); })
+  }
+  return result;
 };
 
 // Do not edit below this line
