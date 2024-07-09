caa2d0370a5ebbcf83a146dae91a1d34d85b4f5d
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-07-10 01:28:33 +0330

    js 07 passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = 5 / 9 * (fahrenheit - 32);
+  return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = celcius * 9 / 5 + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
