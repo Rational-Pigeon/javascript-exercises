ceb9151dd1bc3a67a2c57f34d767f55061a26b42
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-07-10 01:06:03 +0330

    js 05 pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof (num1) != "number" || typeof (num2) != "number"
+    || num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
+  const smaller = num1 < num2 ? num1 : num2;
+  const larger = num1 < num2 ? num2 : num1;
+  let sum = 0;
 
+  for (let i = smaller; i <= larger; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
