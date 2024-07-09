884f873bc0c876f4431d09be22d974bcdb8162d4
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-07-10 00:28:33 +0330

    js 03 pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let stringLength = str.length;
+  let reversedString = ""
+  for (let i = stringLength - 1; i >= 0; i--) {
+    reversedString = reversedString + str.charAt(i);
+  }
+  console.log(reversedString)
+  return reversedString;
 };
 
 // Do not edit below this line
