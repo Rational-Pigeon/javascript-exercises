0986fe003e50237d67ec12b2b1dbaf67148d5457
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-07-17 20:05:59 +0330

    js 12 - done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = (person) => {
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  let age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth :
+    currentYear - person.yearOfBirth;
+  return age;
+}
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    oldest = getAge(oldest) >= getAge(person) ? oldest : person;
+    return oldest;
+  })
 };
 
 // Do not edit below this line
