f44e476249ef87e8406d9873da7271aff63d4926
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-07-17 19:13:23 +0330

    js 09 - done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+// removes punctuation and applies toLowerCase
+function formatString(str) {
+  let formattedString = str.toLowerCase().replace(/\s+/g, '');
+  return formattedString.replace(/[^\w\s]|_/g, '');
+}
 
+const palindromes = function(str) {
+  let formattedString = formatString(str);
+  let reversedString = formattedString.split('').reverse().join('');
+  return reversedString === formattedString;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
